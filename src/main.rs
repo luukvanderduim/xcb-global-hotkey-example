7fcb8d72394f990fab6028cc295aa20d6e076838
Luuk van der Duim <luukvanderduim@gmail.com>
2019-11-06 14:10:32 +0100

    quick n dirty hotkey, but works

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
 use xcb;
 use xcb::ffi::xproto::*;
+use xcb::xproto::*;
 use xcb::xkb;
-//use xcb::xproto;
 use xcb_util::keysyms::KeySymbols;
-
+use x11::keysym::*;
 
 fn main() {
 
-    let (conn, _) = xcb::Connection::connect(None).unwrap();
-   
-    let key_symb: KeySymbols = KeySymbols::new(&conn);
-    let mut keyf12 = key_symb.get_keycode(65481 as u32);
-
+    let (conn, screen_num) = xcb::Connection::connect(None).expect("Connection failed");
     let setup = conn.get_setup();
+    let screen = setup.roots().nth(screen_num as usize).expect("screen setup failed");
+   
+  //  let key_symb: KeySymbols = KeySymbols::new(&conn);
+  //  let mut key = key_symb.get_keycode(xcb::Keysym::default());
     
-    for screen in setup.roots() {
-            let mut rez_grab = xcb::grab_key(&conn, true, screen.root(), keyf12.next().unwrap() as u16 , 0, XCB_GRAB_MODE_ASYNC as u8, XCB_GRAB_MODE_ASYNC as u8);
-            let valuelist = [(XCB_EVENT_MASK_EXPOSURE , XCB_EVENT_MASK_BUTTON_PRESS)];
-            let mut _rez_chatrr = xcb::xproto::change_window_attributes(&conn, screen.root(), &valuelist );
-    }
+  //  for screen in setup.roots() {
 
-    conn.flush();
+            let _ = xcb::grab_key_checked(&conn, true, screen.root(), MOD_MASK_ANY as u16, 96 as u8, GRAB_MODE_ASYNC as u8, GRAB_MODE_ASYNC as u8)
+            .request_check().expect("The key grab failed");
+            let valuelist = [( CW_EVENT_MASK , EVENT_MASK_KEY_PRESS)];
+            let _rez_chatrr = xcb::xproto::change_window_attributes_checked(&conn, screen.root(), &valuelist )
+            .request_check().expect("Change of window attributes failed");
+                   
+  //  }
+
+   
     loop {
+         &conn.flush();
         if let Some(ev) = (&conn).wait_for_event() {
-            if ev.response_type() & !0x80 == XCB_KEY_PRESS {
-                println!("Crtl-F12, Really?!");
-            }
+            if ev.response_type() & !0x80 == KEY_PRESS {
+                println!("Key event");
+            } 
+                        
         }
     }
 }

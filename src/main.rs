1097b53b5fbf85c961c0753d87d75c4a605b9bf7
Luuk van der Duim <luukvanderduim@gmail.com>
2019-11-10 14:15:27 +0100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,27 @@
 use xcb;
-use xcb::ffi::xproto::*;
-use xcb::xproto::*;
-use xcb::xkb;
-use xcb_util::keysyms::KeySymbols;
-use x11::keysym::*;
+use xcb::xproto::{MOD_MASK_ANY, GRAB_MODE_ASYNC, CW_EVENT_MASK, EVENT_MASK_KEY_PRESS, KEY_PRESS};
 
 fn main() {
-
-    let (conn, screen_num) = xcb::Connection::connect(None).expect("Connection failed");
+    let (conn, _) = xcb::Connection::connect(None).expect("Connection failed");
     let setup = conn.get_setup();
-    let screen = setup.roots().nth(screen_num as usize).expect("screen setup failed");
-   
-  //  let key_symb: KeySymbols = KeySymbols::new(&conn);
-  //  let mut key = key_symb.get_keycode(xcb::Keysym::default());
-    
-  //  for screen in setup.roots() {
 
-            let _ = xcb::grab_key_checked(&conn, true, screen.root(), MOD_MASK_ANY as u16, 96 as u8, GRAB_MODE_ASYNC as u8, GRAB_MODE_ASYNC as u8)
-            .request_check().expect("The key grab failed");
-            let valuelist = [( CW_EVENT_MASK , EVENT_MASK_KEY_PRESS)];
-            let _rez_chatrr = xcb::xproto::change_window_attributes_checked(&conn, screen.root(), &valuelist )
-            .request_check().expect("Change of window attributes failed");
-                   
-  //  }
 
-   
+    // A display may consist of more than one screen, all screens have a root window
+   for screen in setup.roots() {
+        xcb::grab_key_checked(&conn, true, screen.root(), MOD_MASK_ANY as u16, 96 as u8, GRAB_MODE_ASYNC as u8, GRAB_MODE_ASYNC as u8)
+                .request_check().expect("The key grab failed");
+        let valuelist = [( CW_EVENT_MASK , EVENT_MASK_KEY_PRESS)];
+        xcb::xproto::change_window_attributes_checked(&conn, screen.root(), &valuelist )
+                .request_check().expect("Change of window attributes failed");           
+   }
+
     loop {
          &conn.flush();
         if let Some(ev) = (&conn).wait_for_event() {
             if ev.response_type() & !0x80 == KEY_PRESS {
-                println!("Key event");
-            } 
-                        
+                println!("F12!");
+                break;
+            }            
         }
     }
 }
